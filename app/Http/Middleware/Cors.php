d437526fefab14d1ae016ae52cb50958f89c5477
danisusanto <danisusanto@niagahoster.co.id>
2020-06-23 18:51:49 +0700

    try again with wildcard cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,10 +16,8 @@ class Cors
     public function handle($request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Credentials', 'true')
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Max-Age', '3600')
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With, Origin, X-Csrftoken, Content-Type, Accept, Authorization');
+            ->header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+            ->header('Access-Control-Allow-Headers', '*');
     }
 }
