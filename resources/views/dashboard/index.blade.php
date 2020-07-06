0f225fa2064ae73eebc9ccabfda664d281bf575a
Mosleim <mosleim@gmail.com>
2020-07-06 19:01:26 +0700

    implementasi real data dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -22,7 +22,7 @@
                 <div class="card-body">
                     <p class="card-title text-md-center text-xl-left">Posts</p>
                     <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-                        <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">34040</h3>
+                        <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">{{ $postCount }}</h3>
                         <i class="ti-files icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
                     </div>
                     <p class="mb-0 mt-2 text-success">100% <span class="text-black ml-1"><small>(nowadays)</small></span></p>
@@ -32,9 +32,9 @@
         <div class="col-md-3 grid-margin stretch-card">
             <div class="card">
                 <div class="card-body">
-                    <p class="card-title text-md-center text-xl-left">Views</p>
+                    <p class="card-title text-md-center text-xl-left">Company</p>
                     <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-                        <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">47033</h3>
+                        <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">{{$companyCount}}</h3>
                         <i class="ti-eye icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
                     </div>
                     <p class="mb-0 mt-2 text-success">100% <span class="text-black ml-1"><small>(current)</small></span></p>
@@ -46,21 +46,9 @@
                 <div class="card-body">
                     <p class="card-title text-md-center text-xl-left">Admins</p>
                     <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-                        <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">40016</h3>
+                        <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">{{$adminCount}}</h3>
                         <i class="ti-user icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
                     </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3 grid-margin stretch-card">
-            <div class="card">
-                <div class="card-body">
-                    <p class="card-title text-md-center text-xl-left">Unique Visitors</p>
-                    <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-                        <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">61344</h3>
-                        <i class="ti-layers-alt icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
-                    </div>
-                    <p class="mb-0 mt-2 text-success">100%<span class="text-black ml-1"><small>(nowadays)</small></span></p>
                 </div>
             </div>
         </div>
