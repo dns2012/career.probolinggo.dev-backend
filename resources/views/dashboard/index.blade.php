e330f7bcbfbe40b0a78337c4c64ae16a2ad0af1e
muhammad bukhori muslim <mosleim@gmail.com>
2020-07-07 19:10:30 +0700

    Adding some of space

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -46,7 +46,7 @@
                 <div class="card-body">
                     <p class="card-title text-md-center text-xl-left">Admins</p>
                     <div class="d-flex flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
-                        <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">{{$adminCount}}</h3>
+                        <h3 class="mb-0 mb-md-2 mb-xl-0 order-md-1 order-xl-0">{{ $adminCount }}</h3>
                         <i class="ti-user icon-md text-muted mb-0 mb-md-3 mb-xl-0"></i>
                     </div>
                 </div>
