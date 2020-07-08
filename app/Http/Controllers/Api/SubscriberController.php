db832318bb30d7edc1907d2306ed8ed482c047d7
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-07-08 22:51:44 +0700

    call subscriber service di store email

--- app/Http/Controllers/Api/SubscriberController.php
+++ app/Http/Controllers/Api/SubscriberController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\SubscriberService;
 use App\Subscriber;
 use App\Tools\ResponseTool;
 use Illuminate\Http\Request;
@@ -12,15 +13,7 @@ class SubscriberController extends Controller
     public function subscribe(Request $request)
     {
         $email = $request->email;
-        $checkEmail = Subscriber::whereEmail($email)->count();
-        if ($checkEmail == 0) {
-            Subscriber::create([
-                'email' => $email
-            ]);
-
-            return ResponseTool::success($email, 201);
-        }
-
-        return ResponseTool::failure("This email has been subscribed");
+        $subscribe = SubscriberService::subscribe($email);
+        return $subscribe;
     }
 }
