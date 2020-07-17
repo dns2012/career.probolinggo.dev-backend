89aa1fe36dd08bebe5cf6ade280d8ea3e08f7b83
Ilham Surya Pratama <ilhamsurya26@gmail.com>
2020-07-18 00:12:32 +0700

    check valid email subscribers

--- app/Http/Controllers/Api/SubscriberController.php
+++ app/Http/Controllers/Api/SubscriberController.php
@@ -7,6 +7,7 @@ use App\Services\SubscriberService;
 use App\Subscriber;
 use App\Tools\ResponseTool;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SubscriberController extends Controller
 {
@@ -17,6 +18,14 @@ class SubscriberController extends Controller
      */
     public function subscribe(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email'
+        ]);
+
+        if ($validator->fails()) {
+            return ResponseTool::failure('The email must be a valid email address.');
+        }
+
         return SubscriberService::subscribe($request->input('email'));
     }
 }
